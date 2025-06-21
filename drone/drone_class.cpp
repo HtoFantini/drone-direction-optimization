c6e1a64b58ec35eb643c96e5fafb196db770d2c2
htofantini <htofantini@gmail.com>
2025-06-21 17:58:24 -0300

    feat: create way to communicate and refactored functions

--- drone/drone_class.cpp
+++ drone/drone_class.cpp
@@ -7,221 +7,182 @@
 #include <cmath>
 #include <unistd.h>
 
-#include "drone_functions.cpp"
-#include "mqtt.cpp"
-#include "nlohmann/json.hpp"   // Para JSON
-
-
-using json = nlohmann::json;
+#include "drone_class.h" // Inclui a definicao da classe
+#include "drone_functions.h" // Suas funcões de geracao aleatória
 
 
 static const double WALK_DISTANCE = 10.0;
 static const float MIN_DIST_TO_ANTENA = 2.0;
 
 
-class Drone {
-private:
-    std::array<double, 2> position;
-    std::array<double, 2> antena_position;
-    float angle;
-    bool reached_antena;
+Drone::Drone(const char *id, const char *host, int port) : mosqpp::mosquittopp(id) {
+    this->position = genInitRandPosition();
+    this->antena_position = {0.0,0.0};
+    this->angle = genInitRandAngle();
+    this->reached_antena = false;
 
-    // TODO: ATRIBUTOS ADICIONADOS SEM CHECAGEM (para o MQTT)
-    pid_t pid;
-    MqttManager mqtt;
+    this->pid = getpid();
+    this->connected_to_broker = false;
+    this->is_sync = false;
 
-
-public:
-    Drone(const std::string& broker_address) : mqtt(broker_address) {
-        this->position = genInitRandPosition();
-        this->antena_position = {0.0,0.0};
-        this->angle = genInitRandAngle();
-        this->reached_antena = false;
-        this->pid = getpid();
-
-        // TODO: AQUI PRA BAIXO NAO ENTENDI NADA, TEM QUE LER
-        // ACHO QUE AQUI VAI TER QUE JOGAR O CONSTRUTOR PRA BAIXO
-        mqtt.set_on_message([this](const std::string& payload) {
-            this->handle_incoming_message(payload);
-        });
-
-        std::string command_topic = "drone/" + std::to_string(pid) + "/para_drone";
-        mqtt.connect_and_subscribe(command_topic);
-    }
-
-    std::array<double, 2> getPosition() {
-        return this->position;
-    }
-
-    void setPosition(std::array<double, 2> position) {
-        this->position = position;
-    }
-
-    std::array<double, 2> getAntenaPosition() {
-        return this->antena_position;
-    }
-
-    void setAntenaPosition(std::array<double, 2> antena_position) {
-        this->antena_position = antena_position;
-    }
-
-    float getAngle() {
-        return this->angle;
-    }
-
-    void setAngle(float angle) {
-        this->angle = angle;
-    }
-
-    bool getReachedAntena() {
-        return this->reached_antena;
-    }
+    this->topic_to_antenna = "drone/" + std::string(id) + "/to_antenna";
+    this->topic_to_drone = "drone/" + std::string(id) + "/to_drone";
     
-    void setReachedAntena(bool reached_antena) {
-        this->reached_antena = reached_antena;
+     // Inicia a conexao com o broker
+    connect(host, port, 60);
+    // Inicia o loop de rede em uma thread separada
+    loop_start();
+}
+
+std::array<double, 2> Drone::getPosition() {return this->position;}
+
+void Drone::setPosition(std::array<double, 2> position) {this->position = position;}
+
+std::array<double, 2> Drone::getAntenaPosition() {return this->antena_position;}
+
+void Drone::setAntenaPosition(std::array<double, 2> antena_position) {this->antena_position = antena_position;}
+
+float Drone::getAngle() {return this->angle;}
+
+void Drone::setAngle(float angle) {this->angle = angle;}
+
+bool Drone::getReachedAntena() {return this->reached_antena;}
+
+void Drone::setReachedAntena(bool reached_antena) {this->reached_antena = reached_antena;}
+
+
+// Evento: conexao estabelecida
+void Drone::on_connect(int rc) {
+    if (rc == 0) {
+        std::cout << "[DRONE " << pid << "] Conectado ao Broker." << std::endl;
+        subscribe(NULL, this->topic_to_drone.c_str());
+    } else {
+        std::cerr << "[DRONE " << pid << "] Falha na conexao." << std::endl;
     }
+}
+
+// Evento: inscricao confirmada
+void Drone::on_subscribe(int mid, int qos_count, const int *granted_qos) {
+    std::cout << "[DRONE " << pid << "] Inscrito para receber comandos." << std::endl;
+    this->connected_to_broker = true;
     
-    // Anda uma quantidade X
-    void walk() {
+    // Envia a mensagem para a antena pedindo para iniciar
+    std::cout << "[DRONE " << pid << "] Enviando pedido de sincronizacao para a central..." << std::endl;
+    json ready_msg;
+    ready_msg["tipo"] = "sync";
+    ready_msg["acao"] = "drone_ready";
+    sendMessage(ready_msg);
+}
 
-        // Aplica uma variacao ao angulo de visada com um valor sorteado dentro de um range
-        // this->angle += uniform_real_distribution(de x ate z)
+// Evento: mensagem recebida
+void Drone::on_message(const struct mosquitto_message *message) {
+    std::string payload(static_cast<char*>(message->payload), message->payloadlen);
+    json msg_json;
+    try { msg_json = json::parse(payload); } catch(...) { return; }
 
-        // Converter o ângulo de graus para radianos
-        double angle_rad = this->angle * M_PI / 180.0;
-
-        // Calcular o deslocamento em X (longitude) e Y (latitude)
-        double delta_x = WALK_DISTANCE * cos(angle_rad);
-        double delta_y = WALK_DISTANCE * sin(angle_rad);
-
-        // Calcular a nova posição somando o deslocamento à posição atual
-        // Assumindo que position[0] = latitude (eixo Y) e position[1] = longitude (eixo X)
-        std::array<double, 2> new_position = {
-            this->position[0] + delta_y,
-            this->position[1] + delta_x
-        };
-
-        // Atualizar a posição do drone
-        this->setPosition(new_position);
-
-        std::cout << "[Drone " << this->pid << "] Andou. "
-                  << "Angulo: " << this->angle << " deg. "
-                  << "Nova Posicao: (Lat: " << new_position[0] 
-                  << ", Lon: " << new_position[1] << ")" << std::endl;
+    // Recebe uma mensagem da central permitindo o inicio
+    if (msg_json.value("tipo", "") == "sync" && msg_json.value("acao", "") == "start_allowed") {
+        std::cout << "[DRONE " << pid << "] Permissao para iniciar recebida!" << std::endl;
+        this->antena_position[0] = msg_json["dados"]["lat"];
+        this->antena_position[1] = msg_json["dados"]["lon"];
+        this->is_sync = true;
     }
+    // Recebe correcao do angulo
+    else if (msg_json.value("tipo", "") == "comando" && msg_json.value("acao", "") == "corrigir_angulo") {
+        std::cout << "[DRONE " << pid << "] Correcao de angulo recebida: " << msg_json["valor"] << std::endl;
+        this->angle = msg_json["valor"];
+    }
+}
 
 
-    // Verifica se esta na mesma posição da antena
-    bool checkEnd() {
-        double dx = getPosition()[1] - getAntenaPosition()[1];
-        double dy = getPosition()[0] - getAntenaPosition()[0];
-        double distance_to_target = std::sqrt(dx*dx + dy*dy);
+void Drone::sendMessage(const json& msg_json) {
+    std::string payload = msg_json.dump();
+    publish(NULL, this->topic_to_antenna.c_str(), payload.length(), payload.c_str());
+}
+
+
+// Anda
+void Drone::walk() {
+
+    // Aplica uma variacao ao angulo de visada com um valor sorteado dentro de um range
+    // this->angle += uniform_real_distribution(de x ate z)
+
+    // Converter o angulo de graus para radianos
+    double angle_rad = this->angle * M_PI / 180.0;
+
+    // Calcular o deslocamento em X (longitude) e Y (latitude)
+    double delta_x = WALK_DISTANCE * cos(angle_rad);
+    double delta_y = WALK_DISTANCE * sin(angle_rad);
+
+    // Calcular a nova posicao somando o deslocamento à posicao atual
+    // Assumindo que position[0] = latitude (eixo Y) e position[1] = longitude (eixo X)
+    std::array<double, 2> new_position = {
+        this->position[0] + delta_y,
+        this->position[1] + delta_x
+    };
+
+    // Atualizar a posicao do drone
+    this->setPosition(new_position);
+
+    std::cout << "[Drone " << this->pid << "] Andou. "
+                << "Angulo: " << this->angle << " deg. "
+                << "Nova Posicao: (Lat: " << new_position[0] 
+                << ", Lon: " << new_position[1] << ")" << std::endl;
+}
+
+
+// Verifica se esta na mesma posicao da antena
+bool Drone::checkEnd() {
+    double dx = getPosition()[1] - getAntenaPosition()[1];
+    double dy = getPosition()[0] - getAntenaPosition()[0];
+    double distance_to_target = std::sqrt(dx*dx + dy*dy);
+    
+    // Verifica se a distancia do target esta dentro do range definido
+    if (distance_to_target < MIN_DIST_TO_ANTENA) {
+        this->reached_antena = true;
+    }
+    return this->reached_antena;
+}
+
+
+// Inicia o Drone
+void Drone::start() {
+// Espera a sincronizacao inicial
+    std::cout << "[DRONE " << pid << "] Aguardando permissao da central..." << std::endl;
+    while (!this->is_sync) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    std::cout << "[DRONE " << pid << "] Sincronizado! Iniciando loop principal." << std::endl;
+
+    // Loop principal
+    while (!this->reached_antena) {
         
-        // Verifica se a distancia do target esta dentro do range definido
-        if (distance_to_target < MIN_DIST_TO_ANTENA) {
-            this->reached_antena = true;
-        }
-        return this->reached_antena
-    }
-
-    // Anda e verifica se esta na mesma posicao da antena
-    bool walkAndCheckEnd() {
+        // Anda e checa se chegou no fim
         walk();
-        return checkEnd();
+        if (checkEnd()) break; // Se chegou, fim do loop
+
+        // Envia posicao e angulo
+        json pos_msg;
+        pos_msg["tipo"] = "posicao";
+        pos_msg["dados"]["lat"] = this->position[0];
+        pos_msg["dados"]["lon"] = this->position[1];
+        pos_msg["dados"]["angulo"] = this->angle;
+        sendMessage(pos_msg);
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        std::cout << "[DRONE " << pid << "] Verificando se houve correcao e andando novamente..." << std::endl;
+
+        // A atualizacao do angulo do drone deve acontecer no maximo ate esta linha
+
+        // Anda com angulo corrigido e checa se chegou no fim
+        walk();
+        if (checkEnd()) break; // Se chegou, fim do loop
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
-    // Envia msg MQTT para a central
-    // TODO: APENAS COPIEI E COLEI, NAO CHEQUEI NADA
-    void sendMessage() {
-        json msg_json;
-        msg_json["tipo"] = "posicao";
-        msg_json["dados"]["lat"] = this->position[0];
-        msg_json["dados"]["lon"] = this->position[1];
-        msg_json["dados"]["angulo"] = this->angle;
-
-        std::string topic = "drone/" + std::to_string(this->pid) + "/para_antena";
-        std::string payload = msg_json.dump();
-
-        std::cout << "[DRONE " << pid << "] Enviando: " << payload << " para o topico " << topic << std::endl;
-        mqtt.publish(topic, payload);
-    }
-
-    // Recebe mensagem MQTT da central
-    // TODO: APENAS COPIEI E COLEI, NAO CHEQUEI NADA
-    json receiveMessage(const std::string& payload) {
-        std::cout << "[DRONE " << pid << "] Mensagem Recebida: " << payload << std::endl;
-        try {
-            return json::parse(payload);
-        } catch (json::parse_error& e) {
-            std::cerr << "[DRONE " << pid << "] Erro ao parsear JSON: " << e.what() << '\n';
-            // Retorna um JSON nulo em caso de erro para evitar que o programa quebre
-            return json(); 
-        }
-    }
-
-    // Recebe a mensagem e atualiza o angulo corrigido
-    // TODO: ELA NAO ESTA RECEBENDO A MSG DIRETAMENTE. TEM QUE VERIFICAR SE ELE PERDE O FATOR ASSINCRONO CASO ISSO ACONTECA
-    void receiveAndUpdate(const json& msg_json) {
-        if (msg_json.is_null()) {
-            return;
-        }
-        // Adicionando checagens de segurança para evitar erros se a chave não existir
-        if (msg_json.contains("tipo") && msg_json.contains("acao") &&
-            msg_json["tipo"] == "comando" && msg_json["acao"] == "corrigir_angulo") {
-            
-            this->angle = msg_json["valor"];
-            std::cout << "[DRONE " << pid << "] Angulo corrigido para: " << this->angle << std::endl;
-        }
-    }
-
-    void handle_incoming_message(const std::string& payload) {
-        json parsed_message = receiveMessage(payload);
-        receiveAndUpdate(parsed_message);
-    }
-
-    // Envia uma mensagem pedindo para iniciar e aguarda a resposta da antena
-    void waitAntenaSync() {
-        // Envia mensagem MQTT para acentral com uma mensagem "Pronto"
-        // while(msg_recebida[msg] != "Ok") {
-        // Fica lendo buffer de recebindo de msg
-        // }
-        // setAntenaPosition(msg_cebida[position])
-    }
-
-
-    // Inicia o Drone
-    void start() {
-
-        waitAntenaSync();
-
-        while(not this->reached_antena){
-
-            walkAndCheckEnd();
-            sendMessage();
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-
-            receiveAndUpdate();
-            walkAndCheckEnd();
-            std::this_thread::sleep_for(std::chrono::seconds(2));
-        }
-    }
-};
-
-// Simulacao do funcionamento
-int main() {
-    Drone d(13);
-
-    // Configura o ponto flutuante para mostrar 4 casas decimais na saída
-    std::cout << std::fixed << std::setprecision(4);
-
-    std::cout << "--- ESTADO INICIAL ---\n";
-    std::cout << "Posicao Inicial: (Lat: " << d.getPosition()[0] << ", Lon: " << d.getPosition()[1] << ")\n";
-    std::cout << "Angulo Inicial: " << d.getAngle() << " graus\n\n";
-
-    d.walk();
-
-    std::cout << "\n--- ESTADO FINAL ---\n";
-    std::cout << "Posicao Final: (Lat: " << d.getPosition()[0] << ", Lon: " << d.getPosition()[1] << ")\n";
-
-    return 0;
+    std::cout << "================================\n";
+    std::cout << "[DRONE " << pid << "] DESTINO ALCANCADO!\n";
+    std::cout << "================================\n";
 }
--- drone/drone_functions.cpp
+++ drone/drone_functions.cpp
@@ -2,15 +2,16 @@
 #include <array>
 #include <random>
 #include <iomanip>
-#include "drone_class.cpp"
 
-// Motor de randomização
+#include "drone_functions.h"
+
+// Motor de randomizacao
 std::mt19937& genRandMotor() {
     static std::mt19937 motor(std::random_device{}());
     return motor;
 }
 
-// Gera uma posição aleatoria
+// Gera uma posicao aleatoria
 std::array<double, 2> genInitRandPosition() {
     std::uniform_real_distribution<double> distrib_latitude(-90.0,90.0);
     std::uniform_real_distribution<double> distrib_longitude(-180.0,180.0);
@@ -25,10 +26,4 @@ std::array<double, 2> genInitRandPosition() {
 float genInitRandAngle() {
     std::uniform_real_distribution<float> distrib_angle(-180.0f,180.0f);
     return distrib_angle(genRandMotor());
-}
-
-// Cria o Drone com nome conforme o PID do processo e inicia sua execução
-void startDrone() {
-    Drone d(13);
-    d.start();
 }
