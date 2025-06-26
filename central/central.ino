03c67ae579ff5dc90059699a1934d417aaf6199f
Matheus Castro <109192244+matheusccosta@users.noreply.github.com>
2025-06-26 18:35:29 -0300

    esp ponto de acesso e principio de comunicacao

--- central/central.ino
+++ central/central.ino
@@ -1,74 +1,87 @@
-#include "sMQTTBroker.h"
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
-sMQTTBroker broker;
+// Configurações do Ponto de Acesso
+const char* AP_SSID = "ESP8266_AP";       
+const char* AP_PASSWORD = "12345678";    
 
-// Use IP na mesma faixa do seu computador
-IPAddress local_IP(192, 168, 56, 1);  // Mude para sua faixa de rede
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 255, 0);    // Máscara correta (/24)
+// Configurações MQTT
+const char* MQTT_BROKER = "192.168.4.2"; // acha com ipconfig dps de conectar na rede do esp
+const int MQTT_PORT = 1883;
+const char* MQTT_CLIENT_ID = "ESP8266_AP_Client";
+const char* MQTT_TOPIC_PUB = "test/esp8266/out";
+const char* MQTT_TOPIC_SUB = "test/esp8266/in";
+const char* MQTT_TOPIC_LED = "test/esp8266/led";
 
-void setup() {
-  delay(2000);
-  Serial.begin(9600);  // Aumente para 115200
-  Serial.println("\n\n--- Iniciando Broker MQTT ---");
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-  // 1. Configuração WiFi
-  WiFi.disconnect(true);
-  delay(1000);
-  WiFi.persistent(false);
-  WiFi.mode(WIFI_STA);  // Fundamental!
-
-  // 2. Configure seu WiFi corretamente
-  const char* ssid = "BLFIBRA URUBU DO PIX ";  // Remova espaço final!
-  const char* password = "apto502blc06";
-
-  // 3. Configuração IP
-  if (!WiFi.config(local_IP, gateway, subnet)) {
-    Serial.println("[ERRO] Falha na configuração IP");
-  } else {
-    Serial.println("[INFO] IP estático configurado");
-  }
-
-  // 4. Conexão WiFi
-  Serial.print("Conectando ao WiFi");
-  WiFi.begin(ssid, password);
+void setup_ap() {
+  Serial.begin(115200);
+  Serial.println("\nConfigurando ponto de acesso...");
   
-  unsigned long start = millis();
-  while (WiFi.status() != WL_CONNECTED) {
-    if (millis() - start > 30000) {
-      Serial.println("\n[ERRO] Timeout WiFi!");
-      ESP.restart();
+  // Cria o ponto de acesso
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(AP_SSID, AP_PASSWORD);
+  
+  Serial.print("SSID: ");
+  Serial.println(AP_SSID);
+  Serial.print("Senha: ");
+  Serial.println(AP_PASSWORD);
+  Serial.print("IP do AP: ");
+  Serial.println(WiFi.softAPIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String message = "";
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+  
+  Serial.print("Mensagem recebida [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  Serial.println(message);
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Conectando ao MQTT Broker...");
+    
+    if (client.connect(MQTT_CLIENT_ID)) {
+      Serial.println("Conectado!");
+      client.subscribe(MQTT_TOPIC_SUB);
+      client.subscribe(MQTT_TOPIC_LED);
+      Serial.println("Inscrito nos tópicos");
+      client.publish(MQTT_TOPIC_PUB, "ESP8266 AP Conectado!");
+    } else {
+      Serial.print("Falha, rc=");
+      Serial.print(client.state());
+      Serial.println(" Tentando novamente em 5s...");
+      delay(5000);
     }
-    Serial.print(".");
-    delay(500);
-  }
-  
-  // 5. Informações de rede
-  Serial.println("\nConectado!");
-  Serial.println("SSID: " + WiFi.SSID());
-  Serial.print("IP local: ");
-  Serial.println(WiFi.localIP());
-  Serial.print("Gateway: ");
-  Serial.println(WiFi.gatewayIP());
-  Serial.print("Máscara: ");
-  Serial.println(WiFi.subnetMask());
-  Serial.print("RSSI: ");
-  Serial.print(WiFi.RSSI());
-  Serial.println(" dBm");
-
-  // 6. Inicialização do Broker
-  Serial.println("Iniciando broker MQTT...");
-  
-  if (broker.init(1883)) {
-    Serial.println("Broker iniciado na porta 1883");
-    Serial.print("Endereço do broker: ");
-    Serial.print(WiFi.localIP());
-    Serial.println(":1883");
-  } else {
-    Serial.println("[ERRO] Falha ao iniciar broker");
   }
 }
 
+void setup() {
+  setup_ap();
+  client.setServer(MQTT_BROKER, MQTT_PORT);
+  client.setCallback(callback);
+}
+
 void loop() {
-  broker.update();
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  // Publica mensagem periódica
+  static unsigned long lastMsg = 0;
+  if (millis() - lastMsg > 10000) {
+    lastMsg = millis();
+    String msg = "Tempo: " + String(millis()/1000) + "s";
+    client.publish(MQTT_TOPIC_PUB, msg.c_str());
+    Serial.println("Enviado: " + msg);
+  }
+  
 }
